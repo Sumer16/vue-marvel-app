2572e796786ab9872619f9254e8ed3ff8212a951
Sumer Patel <sumerdpatel@gmail.com>
2020-08-20 18:14:55 +0530

    used store

--- src/components/Character.vue
+++ src/components/Character.vue
@@ -1,13 +1,35 @@
 <template>
   <div>
     <h3>This is a Marvel Character</h3>
+    <p>{{ this.$route.params.id }}</p>
+    <ul>
+      <li v-for="char in character" :key="char.id">
+        {{ char.name }}
+        {{ char.modified }}
+        {{ char.description }}
+      </li>
+    </ul>
+
+    <img :src="url" alt="" srcset="">
   </div>
 </template>
 
 <script>
-export default {
-  name: 'Character'
+import { marvel_public_key } from '../marvel';
+import { mapState } from 'vuex';
+import axios from 'axios';
 
+export default {
+  name: 'Character',
+  mounted() {
+    this.$store.dispatch('getCharacter', this.$route.params.id);
+  },
+  computed: {
+    ...mapState({
+      character: state => state.character,
+      url: state => state.url
+    })
+  },
 }
 </script>
 
