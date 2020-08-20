fb4bf2897cd51be2f185faa59b7525c7bcc9008d
Sumer Patel <sumerdpatel@gmail.com>
2020-08-20 13:08:42 +0530

    added component-characters

---
+++ src/components/Characters.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h3>{{ msg }}</h3>
+  </div>
+</template>
+
+<script>
+import { marvel_secret_key, marvel_public_key} from '../marvel';
+
+export default {
+  name: 'Characters',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <img class="m-auto" alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Characters msg="Welcome to Your Marvel App in VueJS"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Characters from '@/components/Characters.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Characters
   }
 }
 </script>
